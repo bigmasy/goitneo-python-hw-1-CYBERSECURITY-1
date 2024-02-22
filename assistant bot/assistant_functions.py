35cfadc9355bc116c95a99bee281d91c810c5d99
bigmasy <jura.tymosh@gmail.com>
2024-02-22 09:21:14 +0200

    added decorator input_error

--- assistant bot/assistant_functions.py
+++ assistant bot/assistant_functions.py
@@ -1,42 +1,46 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return 'Give me name and phone please.'
+        except KeyError:
+            return 'Enter correct name please.'
+        except IndexError:
+            return 'Enter user name please.'
+        
+    return inner
+
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+
+@input_error
 def add_contact(args, contacts):
-    if len(args) != 2:
-        return 'Wrong arguments to the command. Try again.'
+    name, phone = args
+    if name in contacts:
+        return 'Contact already exists, if you want to change it, try the change command'
     else:
-        name, phone = args
-        if name in contacts:
-            return 'Contact already exists, if you want to change it, try the change command'
-        else:
-            contacts[name] = phone
-            return 'Contact added'
+        contacts[name] = phone
+        return 'Contact added'
 
+
+@input_error
 def change_contact(args, contacts):
-    if len(args) != 2:
-        return 'Wrong arguments to the command. Try again.'
-    else:
-        name, phone = args
-        if name in contacts:
-            contacts[name] = phone
-            return 'Contact is changed'
-        else:
-            contacts[name] = phone
-            return f'There was no contact with the name {name}, contact added'
+    name, phone = args
+    contacts[name] = phone
+    return 'Contact is changed'
 
+
+@input_error
 def show_phone(args, contacts):
-    if len(args) != 1:
-        return 'Wrong arguments to the command. Try again.'
-    else:
-        name = args[0]
-        if name in contacts:
-            return contacts[name]
-        else:
-            return f'There is no contact with the name {name}'
+    name = args[0]
+    return contacts[name]
+
 
 def show_all_contacts(contacts):
     contacts_list = [f'{name}: {phone}' for name, phone in contacts.items()]
     return '\n'.join(contacts_list)
-
